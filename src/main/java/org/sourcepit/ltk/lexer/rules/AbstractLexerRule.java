8586b1c659b23865a628088c6df9eb2e05e89bf9
Bernd Vogt <bernd.vogt@sourcepit.org>
2017-05-08 22:22:40 +0200

    lexer rule refactoring: AbstractLexerRule#lexeme

--- src/main/java/org/sourcepit/ltk/lexer/rules/AbstractLexerRule.java
+++ src/main/java/org/sourcepit/ltk/lexer/rules/AbstractLexerRule.java
@@ -1,6 +1,5 @@
 package org.sourcepit.ltk.lexer.rules;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.sourcepit.ltk.lexer.symbols.Symbol;
@@ -9,16 +8,12 @@ public abstract class AbstractLexerRule implements LexerRule {
 
 	protected LexemeRef lexeme;
 
-	private List<Symbol> visited;
-
 	public void onStart(List<Symbol> symbolBuffer, int lexemeStart) {
 		lexeme = new LexemeRef();
 		lexeme.setRule(this);
 		lexeme.setSymbolBuffer(symbolBuffer);
 		lexeme.setState(LexemeState.INCOMPLETE);
 		lexeme.setOffset(lexemeStart);
-
-		visited = new ArrayList<Symbol>();
 	}
 
 	@Override
@@ -34,17 +29,6 @@ public abstract class AbstractLexerRule implements LexerRule {
 		}
 
 		lexeme.setLength(lexeme.getLength() + 1);
-
-		if (visited.contains(symbol)) {
-
-			System.out.println(symbol);
-		}
-
-		visited.add(symbol);
-
-		if (lexeme.getLength() != length) {
-			System.out.println();
-		}
 
 		return onSymbol(symbol);
 	}
